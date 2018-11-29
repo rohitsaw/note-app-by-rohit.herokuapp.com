6ef1bf9fad77255283be8a9f68ac09046ea4cc36
rohitsaw <rsaw409@gmail.com>
2018-11-29 20:16:33 +0530

    test

--- noteapp/models.py
+++ noteapp/models.py
@@ -3,7 +3,8 @@ import datetime
 # Create your models here.
 
 class Notes(models.Model):
-    user = models.CharField(max_length=64)
+    # user = models.CharField(max_length=64)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
     heading = models.CharField(max_length=100)
     notes = models.TextField()
     date = models.DateField(default=datetime.date.today)
